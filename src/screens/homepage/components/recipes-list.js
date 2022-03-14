2a2dfd642f1edecd2c65a890e3aeda1320006afb
Ahmad AbuYamen <ayamen@atypon.com>
2022-03-14 10:13:01 +0300

    documenting

--- src/screens/homepage/components/recipes-list.js
+++ src/screens/homepage/components/recipes-list.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import SingleRecipeItem from "./single-recipe-item";
 
 const RecipesList = ({ name, recipes, numToExpect, actionComponent, actionOnClick }) => {
+    /**
+     * A recipes list component that shows a list of recipes together.
+     * @param {string} name - List name, more of a section title to show 
+     * @param {Array} recipes - List of recipe objects
+     * @param {number} numToExpect - (optional) If it expects a number of items (for loading cards purpose only).
+     * @param {Component} actionComponent - A component/icon that shows on the other side of the title.
+     * @param {function} actionOnClick - A callback function that gets called when the `actionComponent` is clicked.
+     */
     return (
         <section className="recipes-list">
             <div className="recipes-list__title-container">
@@ -17,7 +25,7 @@ const RecipesList = ({ name, recipes, numToExpect, actionComponent, actionOnClic
                         // Showing `numToExpect` of loading `SingleRecipeItem` ..
                         [...Array(numToExpect)].map((item, index) => <SingleRecipeItem key={index.toString()} loading />)
                         :
-                        // When avaiable showing the list of recipes
+                        // When avaiable show the list of recipes
                         recipes.map((recipe, index) => (
                             <SingleRecipeItem
                                 key={index.toString()}
